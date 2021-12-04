dff24bca3bf513001232d5b84d6fb0a2c7cc4d3a
Allan <matrixboston7613@gmail.com>
2021-12-04 20:14:15 +0800

    Selective cannon added to only target living ships

--- scripts/index.js
+++ scripts/index.js
@@ -4,12 +4,14 @@ class Ship {
     this.currentHP = startingHP;
     this.decrement = decrement;
     this.shipNumber = shipNumber;
+    this.isDestroyed = false;
   }
-
-  
+ 
   //Will simply update ship
   updateShip() {
     this.currentHP -= this.decrement 
+    if(this.currentHP <= 0){this.isDestroyed = true}
+    //ability to tell if ship is destroyed (will be handy for the gun)
   }
 
   //Will generate an html element of the ship
@@ -19,7 +21,7 @@ class Ship {
             <h1>${this.type} Ship-${this.shipNumber}</h1>
             <h1>${this.currentHP}</h1>
         </div>`;
-    return this.currentHP > 0? profileHTML : "kaboom"
+    return this.isDestroyed? "kaboom": profileHTML 
   }
 
 }
@@ -57,6 +59,18 @@ class Fleet {
 
 //Very very basic logic of game works when tested with a 'click' event listener
 
+//Cannon function (can only hit living alien ships)
+const cannon = (enemyFleet) => {
+  //Make an array of ship numbers from fleet if their this.destroyed = false
+  const availableTargets = enemyFleet.assignedShips.map((ship)=>{
+    if(ship.isDestroyed === false) return ship.shipNumber
+  }).filter((items) => items != undefined)
+  //Choose a random number from that array
+  const randomTarget = availableTargets[Math.floor(Math.random()*availableTargets.length)]
+  //return that random number 
+  return randomTarget
+  //therefore, will only hit ships that aren;t destroyed
+}
 
 
 const body = document.querySelector("body")
@@ -65,8 +79,7 @@ starFleet.buildFleet()
 body.innerHTML = starFleet.entireFleetHTML
 
 document.addEventListener("click", ()=>{
-  const randomNumber = Math.round(Math.floor(Math.random() * 14) + 1);
-  starFleet.updateFleetStatus(randomNumber)
+  starFleet.updateFleetStatus(cannon(starFleet))
   body.innerHTML=starFleet.entireFleetHTML
 })
 
