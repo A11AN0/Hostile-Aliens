6036a16a6797f56ba89a0723a6024a78101d6286
Allan <matrixboston7613@gmail.com>
2022-02-17 12:01:34 +0800

    reload url

--- main.js
+++ main.js
@@ -1,32 +1,36 @@
-import {Fleet, cannon} from './scripts/index.js'
+import { Fleet, cannon } from "./scripts/index.js";
 /*----------------------------------------------------------------------------*/
 //MAIN GAME
 /*----------------------------------------------------------------------------*/
 
-const arena = document.querySelector(".arena")
-const button = document.querySelector(".cannon__button")
-const starFleet = new Fleet
-starFleet.buildFleet()
-arena.innerHTML = starFleet.entireFleetHTML
+const arena = document.querySelector(".arena");
+const button = document.querySelector(".cannon__button");
+const starFleet = new Fleet();
+starFleet.buildFleet();
+arena.innerHTML = starFleet.entireFleetHTML;
 
-const isGameOver = () =>{
-  const motherShip = document.querySelector(".Ship-1")
-  if(starFleet.assignedShips[0].isDestroyed === true){
-    const restart = confirm("Mother ship destroyed, would you like to play again?");
-    if(restart === true){window.location = "/"}
+const isGameOver = () => {
+  const motherShip = document.querySelector(".Ship-1");
+  if (starFleet.assignedShips[0].isDestroyed === true) {
+    const restart = confirm(
+      "Mother ship destroyed, would you like to play again?"
+    );
+    if (restart === true) {
+      window.location = window.location.href;
+    }
   }
-}
+};
 
-button.addEventListener("click", ()=>{
-  starFleet.updateFleetStatus(cannon(starFleet))
-  arena.innerHTML=starFleet.entireFleetHTML
-  isGameOver()
-})
+button.addEventListener("click", () => {
+  starFleet.updateFleetStatus(cannon(starFleet));
+  arena.innerHTML = starFleet.entireFleetHTML;
+  isGameOver();
+});
 
-document.addEventListener("keydown", (event)=>{
-  if(event.key === " "){
-    starFleet.updateFleetStatus(cannon(starFleet))
-    arena.innerHTML=starFleet.entireFleetHTML
-    isGameOver()
+document.addEventListener("keydown", (event) => {
+  if (event.key === " ") {
+    starFleet.updateFleetStatus(cannon(starFleet));
+    arena.innerHTML = starFleet.entireFleetHTML;
+    isGameOver();
   }
-})
+});
